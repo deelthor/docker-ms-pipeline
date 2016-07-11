a65f18ad7822952692778586f50ddbef4adebf03
Deelmann Thorsten <thorsten.deelmann@metro.de>
2016-07-11 14:34:53 +0200

    code formatting

--- sample-microservice/src/main/java/de/deelthor/pipeline/controller/UserController.java
+++ sample-microservice/src/main/java/de/deelthor/pipeline/controller/UserController.java
@@ -1,10 +1,11 @@
 package de.deelthor.pipeline.controller;
 
-import de.deelthor.pipeline.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import de.deelthor.pipeline.service.UserService;
 
 @RestController
 public class UserController {
--- sample-microservice/src/test/java/de/deelthor/pipeline/service/UserServiceTest.java
+++ sample-microservice/src/test/java/de/deelthor/pipeline/service/UserServiceTest.java
@@ -1,27 +1,27 @@
 package de.deelthor.pipeline.service;
 
-import org.junit.Test;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
 
 public class UserServiceTest {
 
     @Test
-    public void echoUserName(){
+    public void echoUserName() {
         UserService userService = new UserService();
         String userName = userService.getUser("peter");
         assertThat(userName, is("peter"));
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void usernameEmpty(){
+    public void usernameEmpty() {
         UserService userService = new UserService();
         userService.getUser("");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void usernameNull(){
+    public void usernameNull() {
         UserService userService = new UserService();
         userService.getUser(null);
     }
