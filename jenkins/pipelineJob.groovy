d1b0ec80358689f1850cd145c13cc37dea544e15
Deelmann Thorsten <thorsten.deelmann@metro.de>
2016-07-12 12:29:05 +0200

    checkout and buld

--- jenkins/pipelineJob.groovy
+++ jenkins/pipelineJob.groovy
@@ -1,6 +1,16 @@
-node {
-  stage 'Stage 1'
-  echo 'Hello World 1'
-  stage 'Stage 2'
-  echo 'Hello World 2'
+node('microservice-sample') {
+  
+  stage 'CHECKOUT'
+  
+  deleteDir()
+ 
+  retry(5) {
+    git url: "https://github.com/deelthor/docker-ms-pipeline", branch: "master", poll: true"
+  }  
+  
+  stage 'BUILD'
+  
+  dir('sample-microservice') {
+    sh "mvn -Dmaven.test.failure.ignore clean package"
+  }
 }
