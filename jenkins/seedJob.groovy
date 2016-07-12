15a98028a8b35f6185a3242be8c886e67eb801dd
Deelmann Thorsten <thorsten.deelmann@metro.de>
2016-07-12 13:38:27 +0200

    adjusted seed job

--- jenkins/seedJob.groovy
+++ jenkins/seedJob.groovy
@@ -1,8 +1,10 @@
-pipelineJob('pipeline') {
+pipelineJob('microservice-sample-pipeline') {
     definition {
-        cps {
-            script("node {\n stage 'Stage 1'\n echo 'Hello World 1'\n stage 'Stage 2'\n echo 'Hello World 2'\n}")
-            sandbox()
+        cpsScm{
+          scm{
+          	git('https://github.com/deelthor/docker-ms-pipeline.git')
+          }
+          scriptPath('jenkins/pipelineJob.groovy')	
         }
     }
 }
